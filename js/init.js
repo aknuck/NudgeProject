1b12e25e7ae4ee5bb46bc8d97556804cc9867b04
Deep Sheth <djsdeep@gmail.com>
2015-04-25 16:50:55 -0400

    alpha release

--- js/init.js
+++ js/init.js
@@ -1,7 +1,12 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
-
+    $('.button-collapse').sideNav();    
+//    $('#newest-tab').click(function() {
+//        window.location.replace('newest.html'); 
+//    });
+//      $('#urgent-tab').click(function() {
+//          window.location.replace('urgent.html');
+//      })
   }); // end of document ready
 })(jQuery); // end of jQuery name space
